62d12d58ec751c4961b455d3c7ea05a651a74556
LDprg <lukas_4dr@gmx.at>
2025-05-22 17:21:52 +0200

    add actuall bench mark code

--- build.zig
+++ build.zig
@@ -15,6 +15,9 @@ pub fn build(b: *std.Build) void {
     // set a preferred release mode, allowing the user to decide how to optimize.
     const optimize = b.standardOptimizeOption(.{});
 
+    const opts = .{ .target = target, .optimize = optimize };
+    const zbench_module = b.dependency("zbench", opts).module("zbench");
+
     // We will also create a module for our other entry point, 'main.zig'.
     const exe_mod = b.createModule(.{
         // `root_source_file` is the Zig "entry point" of the module. If a module
@@ -32,6 +35,8 @@ pub fn build(b: *std.Build) void {
         .name = "lcs_zig",
         .root_module = exe_mod,
     });
+
+    exe.root_module.addImport("zbench", zbench_module);
 
     // This declares intent for the executable to be installed into the
     // standard location when the user invokes the "install" step (the default
--- src/main.zig
+++ src/main.zig
@@ -1,24 +1,94 @@
 const std = @import("std");
+const zbench = @import("zbench");
+
+const LCSBenchmark = struct {
+    loops: usize,
+    func: *const fn (std.mem.Allocator, str1: []const u8, str2: []const u8) std.mem.Allocator.Error![]const u8,
+    str1: []const u8,
+    str2: []const u8,
+
+    fn init(
+        loops: usize,
+        func: *const fn (std.mem.Allocator, str1: []const u8, str2: []const u8) std.mem.Allocator.Error![]const u8,
+        str1: []const u8,
+        str2: []const u8,
+    ) LCSBenchmark {
+        return .{
+            .loops = loops,
+            .func = func,
+            .str1 = str1,
+            .str2 = str2,
+        };
+    }
+
+    pub fn run(self: LCSBenchmark, alloc: std.mem.Allocator) void {
+        for (0..self.loops) |_| {
+            std.mem.doNotOptimizeAway(self.func(alloc, self.str1, self.str2));
+        }
+    }
+};
 
 pub fn main() !void {
-    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
-    const alloc = gpa.allocator();
-
     const str1 = "abcdxyz";
     const str2 = "xyzabcd";
 
-    const res = try refLCS(alloc, str1, str2);
+    const alloc = std.heap.page_allocator;
+    const stdout = std.io.getStdOut().writer();
+    var bench = zbench.Benchmark.init(alloc, .{});
+    defer bench.deinit();
 
-    std.debug.print("Str1: \"{s}\"\nStr2: \"{s}\"\nRes: \"{s}\"\n", .{ str1, str2, res });
+    try bench.addParam("Reference impl.", &LCSBenchmark.init(100_00, refLCS, str1, str2), .{
+        .iterations = 10,
+        .track_allocations = true,
+        .use_shuffling_allocator = true,
+    });
+    try bench.addParam("Lower memory impl.", &LCSBenchmark.init(100_00, lessSpaceLCS, str1, str2), .{
+        .iterations = 10,
+        .track_allocations = true,
+        .use_shuffling_allocator = true,
+    });
+
+    try stdout.writeAll("\n");
+
+    try zbench.prettyPrintHeader(stdout);
+    var iter = try bench.iterator();
+
+    const root_node = std.Progress.start(.{ .root_name = "Benchmarks running...", .estimated_total_items = iter.remaining.len });
+    defer root_node.end();
+    const runs_node = root_node.start("Runs", iter.remaining[0].config.iterations);
+    defer runs_node.end();
+    var current_node = root_node.start(iter.remaining[0].name, 0);
+    defer current_node.end();
+
+    while (try iter.next()) |step| switch (step) {
+        .progress => |i| {
+            current_node.end();
+            current_node = root_node.start(iter.remaining[0].name, 0);
+            root_node.setEstimatedTotalItems(i.total_benchmarks);
+            root_node.setCompletedItems(i.completed_benchmarks);
+            runs_node.setEstimatedTotalItems(i.total_runs);
+            runs_node.setCompletedItems(i.completed_runs);
+        },
+        .result => |x| {
+            defer x.deinit();
+            try x.prettyPrint(alloc, stdout, true);
+        },
+    };
 }
 
-pub fn refLCS(alloc: std.mem.Allocator, str1: []const u8, str2: []const u8) ![]const u8 {
+pub fn refLCS(alloc: std.mem.Allocator, str1: []const u8, str2: []const u8) std.mem.Allocator.Error![]const u8 {
     var table: [][]usize = try alloc.alloc([]usize, str1.len + 1);
     for (table) |*row| {
         row.* = try alloc.alloc(usize, str2.len + 1);
         for (row.*) |*elem| {
             elem.* = 0;
         }
+    }
+    defer {
+        for (table) |*row| {
+            alloc.free(row.*);
+        }
+        alloc.free(table);
     }
 
     for (str1, 0..) |c1, i| {
@@ -44,6 +114,59 @@ pub fn refLCS(alloc: std.mem.Allocator, str1: []const u8, str2: []const u8) ![]c
             i -= 1;
             j -= 1;
         } else if (table[i - 1][j] >= table[i][j - 1]) {
+            i -= 1;
+        } else {
+            j -= 1;
+        }
+    }
+
+    return out;
+}
+
+pub fn lessSpaceLCS(alloc: std.mem.Allocator, str1: []const u8, str2: []const u8) std.mem.Allocator.Error![]const u8 {
+    var table: [][]usize = try alloc.alloc([]usize, str1.len);
+    for (table) |*row| {
+        row.* = try alloc.alloc(usize, str2.len);
+        for (row.*) |*elem| {
+            elem.* = 0;
+        }
+    }
+    defer {
+        for (table) |*row| {
+            alloc.free(row.*);
+        }
+        alloc.free(table);
+    }
+
+    for (str1, 0..) |c1, i| {
+        for (str2, 0..) |c2, j| {
+            if (c1 == c2) {
+                if (i > 0 and j > 0) {
+                    table[i][j] = table[i - 1][j - 1] + 1;
+                } else {
+                    table[i][j] = 1;
+                }
+            } else {
+                const l = if (i > 0) table[i - 1][j] else 0;
+                const r = if (j > 0) table[i][j - 1] else 0;
+                table[i][j] = @max(l, r);
+            }
+        }
+    }
+
+    var out = try alloc.alloc(u8, table[str1.len - 1][str2.len - 1]);
+    var idx = out.len;
+
+    var i = str1.len;
+    var j = str2.len;
+
+    while (i > 0 and j > 0) {
+        if (str1[i - 1] == str2[j - 1]) {
+            idx -= 1;
+            out[idx] = str1[i - 1];
+            i -= 1;
+            j -= 1;
+        } else if (table[i - 2][j - 1] >= table[i - 1][j - 2]) {
             i -= 1;
         } else {
             j -= 1;
